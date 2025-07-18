d8f8d2d9525f6156774694903a293f2ca759b909
Viktor <bektop80@gmail.com>
2025-07-18 11:31:57 +0500

    bot

--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ from aiogram.filters.command import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = '7565743468:AAGaIxF-FewIPTBMCSjukdYw2epjh7U3zlY'
